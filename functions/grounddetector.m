acb53e4b9d00a605b1fc4ffbf1ef1a315b6bffd0
George Fahmy <george@kespry.com>
2016-09-11 10:58:15 -0700

    more updates to the ground detector script

--- functions/grounddetector.m
+++ functions/grounddetector.m
@@ -10,52 +10,62 @@ ttime = [0:1/100:(length(throttle)-1)/100]';
 velocity = emb_state.velocity_z;
 vtime = [0:1/100:(length(velocity)-1)/100]';
 
-accel = movingmean(accel,10);
-gyro = movingmean(gyro,10);
+accel = movingmean(accel,6);
+gyro = movvar(gyro,10);
 
 vector_lengths = cellfun(@(x) length(x),{accel gyro throttle velocity});
 time = [0:1/100:(min(vector_lengths)-1)/100]';
 
 switch min(vector_lengths)
     case vector_lengths(1)
+        c = 1;
         [gtime,gyro] = interpOp(gtime,gyro,atime,accel,'');
         [ttime,throttle] = interpOp(ttime,throttle,atime,accel,'');
         [vtime,velocity] = interpOp(vtime,velocity,atime,accel,'');
         
     case vector_lengths(2)
+        c = 2;
         [atime,accel] = interpOp(atime,accel,gtime,gyro,'');
         [ttime,throttle] = interpOp(ttime,throttle,gtime,gyro,'');
         [vtime,velocity] = interpOp(vtime,velocity,gtime,gyro,'');
         
     case vector_lengths(3)
+        c = 3;
         [atime,accel] = interpOp(atime,accel,ttime,throttle,'');
         [gtime,gyro] = interpOp(gtime,gyro,ttime,throttle,'');
         [vtime,velocity] = interpOp(vtime,velocity,ttime,throttle,'');
 
     case vector_lengths(4)
+        c = 4;
         [atime,accel] = interpOp(atime,accel,vtime,velocity,'');
         [gtime,gyro] = interpOp(gtime,gyro,vtime,velocity,'');
         [ttime,throttle] = interpOp(ttime,throttle,vtime,velocity,'');
 end
 
-gdtimer = 0;
-for i = 1:length(time)
-    if  gyro(i) > 1.4 && accel(i) < -1.3 && throttle(i) < .55
-        gdtimer(i) = time(i);
-    end
+%%%%%%%%%%
+gd = 0;
+for i = length(time)-100*100:length(time)
+    if abs(velocity(i)) < .1 && throttle(i) < .53
+        gd(i) = time(i);
         
+        detect = 1;
+    else
+        gd(i) = 0;
+    end 
 end
+gd = gd(gd~=0);
+gd = gd(10); % 10 milisecond wait
+%%%%%%%%%%
 
-gdtime = gdtimer(gdtimer ~= 0);
-if isempty(gdtime)
-    fprintf('ground detector empty\n')
-else
-    gdtime = gdtime(1);
-
+for i = 1:length(time)
+    
+end
+    
     figure;
     plot(atime,accel,gtime,gyro,ttime,throttle,vtime,velocity);
     grid on;
     xlabel('time (sec)');
     legend('accel','gyro','throttle','velocity z','Location','best');
-    vline(gdtime,'r','gd');
-end
+    vline(gd,'r','gd');
+    
+
