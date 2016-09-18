4e1d4cf79c52c9f8200cff566c6a4cb13c0387df
George Fahmy <george@kespry.com>
2016-09-18 10:17:38 -0700

    updated tracklog script to allow for more robust usage

--- myotherstuff/tracklog.m
+++ myotherstuff/tracklog.m
@@ -1,9 +1,25 @@
 %% tracklog analysis
-function [data] = tracklog(file)
+function tracklog(file)
+
+if exist('file','var')
+    assignin('base','file',file);
+else 
+    if ~exist('file','var')
+        cd '~/Desktop/tracklogs'
+        file = uigetfile('*.*','Select the tracklog to analyze');
+        assignin('base','file',file);
+        if file == 0
+            close all;
+            clc;
+            fprintf('User selected Cancel\n');
+            return
+        end
+    end
+end
  
 fid = fopen(file,'r');
-header = fgetl(fid);
-header = fgetl(fid);
+header = fgetl(fid); % I have to do this 3 times to get to the correct line
+header = fgetl(fid); % in the file
 header = fgetl(fid);
 header = regexprep(header,' ','_');
 header = strsplit(header,',')';
@@ -16,3 +32,6 @@ raw = textscan(fid,[str '%[^\n\r]'],'Delimiter',',','Headerlines',3);
 for i = 1:length(header)
     data.(cell2mat(header(i))) = cell2mat(raw(i));
 end
+assignin('base',file,data);
+fprintf('loaded %s\n',file)
+
