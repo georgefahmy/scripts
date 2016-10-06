372b57aa1da5063c6d45fdfdfe0b599ce58a1c41
George Fahmy <george@kespry.com>
2016-10-05 17:49:03 -0700

    added battery_ to the uid portion for clarity as well as formatted the comments.

--- functions/importsummary.m
+++ functions/importsummary.m
@@ -24,21 +24,21 @@ function mission_summary = importsummary(file)
     
 %%
 
-mission_summary.start_time = mission_summary_file(ind1);         %start_time
+mission_summary.start_time = mission_summary_file(ind1);                    %start_time
 mission_summary.start_time = mission_summary.start_time{1};
-mission_summary.flight_id = mission_summary_file(ind2);               %flight_id
+mission_summary.flight_id = mission_summary_file(ind2);                     %flight_id
 mission_summary.flight_id = mission_summary.flight_id{1};
-mission_summary.drone_name = mission_summary_file(ind3);              %drone_name
+mission_summary.drone_name = mission_summary_file(ind3);                    %drone_name
 mission_summary.drone_name = mission_summary.drone_name{1};
-mission_summary.duration = mission_summary_file{ind4};                %duration
-mission_summary.flight_end_state = mission_summary_file{ind5};        %flight_end_state
-mission_summary.flight_end_reason = mission_summary_file{ind13};
-mission_summary.mission_name = mission_summary_file{ind6};            %mission_name
-mission_summary.battery_info = mission_summary_file{ind7};           %battery_info
-mission_summary.sw_version = mission_summary_file{ind8};  %drone_software_version
-mission_summary.fly_height = mission_summary_file{ind9};            %drone_height
-mission_summary.imaging_height = mission_summary_file{ind10};         %imaging_height
-mission_summary.user_email = mission_summary_file{ind11};             %user_email
+mission_summary.duration = mission_summary_file{ind4};                      %duration
+mission_summary.flight_end_state = mission_summary_file{ind5};              %flight_end_state
+mission_summary.flight_end_reason = mission_summary_file{ind13};            %flight_end_reason
+mission_summary.mission_name = mission_summary_file{ind6};                  %mission_name
+mission_summary.battery_info = ['battery_' mission_summary_file{ind7}];     %battery_info
+mission_summary.sw_version = mission_summary_file{ind8};                    %drone_software_version
+mission_summary.fly_height = mission_summary_file{ind9};                    %drone_height
+mission_summary.imaging_height = mission_summary_file{ind10};               %imaging_height
+mission_summary.user_email = mission_summary_file{ind11};                   %user_email
 
 %disp(mission_summary);
 
