6624b6afcf11e53e8d33aba82c6e4b2163bd6bc9
George Fahmy <george@kespry.com>
2016-10-09 18:13:36 -0700

    updated lidarplotter to use position to determine if flying or not

--- functions/lidarplotter.m
+++ functions/lidarplotter.m
@@ -1,13 +1,33 @@
 function lidarplotter(file)
-% lidar plotter
+%% lidar plotter
 %
-lidar = importdata(file);
+if ~exist('lidar','var')
+    lidar = importdata(file);
+end
+
 raw_dist = lidar.raw_distance;
+pos_z = -emb_state.translation_z;
 
 sample_size = length(raw_dist);
+time = [1:1:sample_size];
+
+flying = time(pos_z >= max(pos_z)-20);
+
+temp1 = time(flying(1));
+temp2 = time(flying(end));
+
+first_sample = temp1;
+last_sample = temp2;
+
 count = 0;
 
 for i = 1:length(raw_dist)
+    if raw_dist(i) >= 51
+        raw_dist(i) = 51;
+    end
+end
+
+for i = first_sample:length(raw_dist(first_sample:last_sample))
     if raw_dist(i) < 51
         count = count+1;
     end
@@ -17,6 +37,6 @@ noise = (count/sample_size)*100;
 fprintf('%.3f percent of lidar data below 51m\n',noise);
 
 figure('Name','Lidar Raw Distance');
-plot(raw_dist);
+scatter([1:1:sample_size],raw_dist,'.');
 grid on;
 
